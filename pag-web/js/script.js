aff8c261eb166d6a6b877a6990cc0c60eb73aeff
Miguel Diaz <miguelitodiaz169@gmail.com>
2024-05-26 20:03:46 -0500

    Agregar trailer a la web

--- pag-web/js/script.js
+++ pag-web/js/script.js
@@ -28,3 +28,17 @@ document.querySelector('.menu').onclick = function (){
     this.classList.toggle('active');
     navigation.classList.toggle('active');
 }
+
+document.querySelectorAll('#btn').forEach(button => {
+    button.addEventListener('click', function(event) {
+        event.preventDefault(); // Evita el comportamiento predeterminado del enlace
+        const target = document.querySelector(this.getAttribute('href'));
+
+        if (target) {
+            window.scrollTo({
+                top: target.offsetTop,
+                behavior: 'smooth' // Añade la animación suave
+            });
+        }
+    });
+});
